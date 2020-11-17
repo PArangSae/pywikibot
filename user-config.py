caf1401e71a81d11e681a6d6adfdea907aa33b94
PrS | 파랑새 <72248972+PArangSae@users.noreply.github.com>
2020-11-17 17:18:18 +0900

    Update and rename user-config.py.sample to user-config.py

--- user-config.py
+++ user-config.py
@@ -3,6 +3,6 @@
 # This is a sample file. You should use generate_user_files.py
 # to create your user-config.py file.
 
-mylang = 'en'
+mylang = 'ko'
 family = 'wikipedia'
-usernames['wikipedia']['en'] = 'Test'
+usernames['wikipedia']['ko'] = 'PArangSae'
